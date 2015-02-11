363250cc6d8c357e8390703566f6ccc1b702ab2a
francisco pagliaricci <franciscopagliaricci@gmail.com>
2015-02-11 18:21:42 -0300

    Added sockets management

--- WpfInterface/WpfInterface/MainWindow.xaml.cs
+++ WpfInterface/WpfInterface/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Kinect;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.IO;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace WpfInterface
 {
@@ -21,10 +26,12 @@ namespace WpfInterface
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static ConcurrentBag<NetworkStream> listenerStreams = new ConcurrentBag<NetworkStream>();
 
         public MainWindow()
         {
             InitializeComponent();
+            setUpSocket();
 
             KinectSensor sensor = KinectSensor.KinectSensors.Where(s => s.Status == KinectStatus.Connected).FirstOrDefault();
 
@@ -69,8 +76,118 @@ namespace WpfInterface
                 if (skel.TrackingState == SkeletonTrackingState.Tracked)
                 {
                     DrawingUtils.DrawSkeleton(skeletonCanvas, skel);
+                    fixSkeleton(skel);
                 }
             }            
+        }
+
+        #region ThreadServer
+
+        private class ThreadServer
+        {
+            private TcpListener serverSocket;
+            private ConcurrentBag<NetworkStream> listenerStreams;
+
+            public ThreadServer(TcpListener serverSocket, ConcurrentBag<NetworkStream> listenerStreams)
+            {
+                this.serverSocket = serverSocket;
+                this.listenerStreams = listenerStreams;
+            }
+
+            public void run()
+            {
+                while (true)
+                {
+                    TcpClient clientSocket = default(TcpClient);
+                    //Locking in accept, waiting to get a new client
+                    clientSocket = serverSocket.AcceptTcpClient();
+                    Debug.WriteLine(" >> Accept connection from client");
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    //Added it to the list of streams I'm communicating with
+                    listenerStreams.Add(networkStream);
+                }
+            }
+        }
+
+
+        private static void setUpSocket()
+        {
+            Debug.WriteLine("SETTING UP SOCKET");
+            TcpListener serverSocket = new TcpListener(8888);
+            serverSocket.Start();
+            Debug.WriteLine(" >> Server Started");
+            Thread thread = new Thread(new ThreadStart(new ThreadServer(serverSocket, listenerStreams).run));
+            thread.Start();
+        }
+
+        private static void fixSkeleton(Skeleton skeleton)
+        {
+            Array types = Enum.GetValues(typeof(JointType));
+
+            string[] lines = new string[types.Length + 1];
+            lines[0] = "SKELETON:" + skeleton.TrackingId + " -> " + skeleton.TrackingState;
+            int i = 1;
+            foreach (JointType type in types)
+            {
+                lines[i++] = getFileFormat(type, skeleton);
+            }
+
+            informListeners(lines);
+        }
+
+        private static void informListeners(string[] lines)
+        {
+            List<NetworkStream> fuckedStreams = new List<NetworkStream>();
+            foreach (NetworkStream stream in listenerStreams)
+            {
+                try
+                {
+                    StringBuilder all = new StringBuilder();
+                    foreach (string line in lines)
+                    {
+                        all.AppendLine(line);
+                    }
+                    // Communication protocol: serialize the skeleton
+                    // Send the size of the package first (packages have variable size)
+                    // Send the serialized skeleton afterwards
+
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(all.ToString());
+                    //Debug.WriteLine("SENDING to: " + stream + "length: " + sendBytes.Length);
+                    Byte[] length = BitConverter.GetBytes(sendBytes.Length);
+                    stream.Write(length, 0, length.Length);
+                    //Debug.WriteLine("LENGTH LENGTH IS :" + length.Length);
+                    stream.Write(sendBytes, 0, sendBytes.Length);
+                    stream.Flush();
+                }
+                catch (IOException)
+                {
+                    //IOException can happen for multiple reasons, but basically, that channel
+                    //has problems and I don't want it, kicking it out. If you want to reconnect,
+                    //just get a new one.
+                    fuckedStreams.Add(stream);
+                }
+            }
+            foreach (NetworkStream stream in fuckedStreams)
+            {
+                NetworkStream temp = stream;
+                temp.Dispose();
+                listenerStreams.TryTake(out temp);
+            }
+        }
+
+        #endregion
+
+        private static string getFileFormat(JointType type, Skeleton skeleton)
+        {
+            Vector4 actualPos = getPosition(type, skeleton);
+
+            return type + " + " + skeleton.Joints[type].TrackingState + " -> " + 
+                actualPos.X + "|" + actualPos.Y + "|" + actualPos.Z + "|" + actualPos.W;
+        }
+
+        public static Vector4 getPosition(JointType type, Skeleton skel)
+        {
+            return skel.BoneOrientations[type].HierarchicalRotation.Quaternion;
         }
     }
 }
